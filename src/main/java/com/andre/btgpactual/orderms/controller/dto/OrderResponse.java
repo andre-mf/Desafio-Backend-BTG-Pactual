8cfc78fd1b689a1d3d3c5554f923f79905bc6f54
andre-mf <andremendesfelix@gmail.com>
2024-10-19 14:02:22 -0300

    Desafio Backend do BTG Pactual - Canal Build & Run

---
+++ src/main/java/com/andre/btgpactual/orderms/controller/dto/OrderResponse.java
@@ -0,0 +1,12 @@
+package com.andre.btgpactual.orderms.controller.dto;
+
+import com.andre.btgpactual.orderms.entity.OrderEntity;
+
+import java.math.BigDecimal;
+
+public record OrderResponse(Long orderId, Long customerId, BigDecimal total) {
+
+    public static OrderResponse fromEntity(OrderEntity entity) {
+        return new OrderResponse(entity.getOrderId(), entity.getCustomerId(), entity.getTotal());
+    }
+}
